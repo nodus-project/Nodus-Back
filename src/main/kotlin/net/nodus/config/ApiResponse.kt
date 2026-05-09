0dec220c1cc007d22185024036f301c22d6c85a6
mini0192 <parkgw9071@gmail.com>
2026-05-09 18:07:47 +0900

    Feat: 프론트랑 맞춤

--- src/main/kotlin/net/nodus/config/ApiResponse.kt
+++ src/main/kotlin/net/nodus/config/ApiResponse.kt
@@ -5,27 +5,27 @@ import net.nodus.global.error.ErrorCode
 
 data class ApiResponse<T> (
     val msg: String? = null,
-    val errorCode: ErrorCode,
+    val errorCode: Int,
     val payload: T?
 ) {
     companion object {
         fun success(): ApiResponse<Void> {
-            return ApiResponse(msg = "Success", errorCode = ErrorCode.SUCCESS, payload = null)
+            return ApiResponse(msg = "Success", errorCode = ErrorCode.SUCCESS.code, payload = null)
         }
         fun <T> success(payload: T?): ApiResponse<T> {
-            return ApiResponse(msg = "Success", errorCode = ErrorCode.SUCCESS, payload = payload)
+            return ApiResponse(msg = "Success", errorCode = ErrorCode.SUCCESS.code, payload = payload)
         }
 
         fun <T> success(payload: T?, message: String): ApiResponse<T> {
-            return ApiResponse(msg = message, errorCode = ErrorCode.SUCCESS, payload = payload)
+            return ApiResponse(msg = message, errorCode = ErrorCode.SUCCESS.code, payload = payload)
         }
 
         fun error(msg: String, errorCode: ErrorCode): ApiResponse<Void> {
-            return ApiResponse(msg = msg, errorCode = errorCode, payload = null)
+            return ApiResponse(msg = msg, errorCode = errorCode.code, payload = null)
         }
 
         fun error(exception: GlobalException): ApiResponse<Void> {
-            return ApiResponse(msg = exception.message, errorCode = exception.errorCode, payload = null)
+            return ApiResponse(msg = exception.message, errorCode = exception.errorCode.code, payload = null)
         }
     }
 }
